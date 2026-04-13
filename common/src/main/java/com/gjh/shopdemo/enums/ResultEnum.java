0d359f198826e6a3b5395346e70be96a34e60c17
guanjianheng <jianheng.guan@govee.com>
2026-04-13 15:01:01 +0800

    引入nacos依赖

---
+++ common/src/main/java/com/gjh/shopdemo/enums/ResultEnum.java
@@ -0,0 +1,21 @@
+package com.gjh.shopdemo.enums;
+
+import lombok.Getter;
+
+/**
+ * 返回结果枚举
+ */
+
+@Getter
+public enum ResultEnum {
+    SUCCESS(1, "成功"),
+    FAIL(0, "失败");
+
+    private Integer code;
+    private String message;
+
+    ResultEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
---
+++ common/src/main/java/com/gjh/shopdemo/pojo/result/ShopResult.java
@@ -0,0 +1,45 @@
+package com.gjh.shopdemo.pojo.result;
+
+import com.gjh.shopdemo.enums.ResultEnum;
+import lombok.Data;
+
+/**
+ * 统一返回结果
+ * @param <T>
+ */
+
+@Data
+public class ShopResult<T> {
+    private Integer code;
+    private String msg;
+    private T data;
+
+    public ShopResult(T data, ResultEnum resultEnum){
+        this.code = resultEnum.getCode();
+        this.msg = resultEnum.getMessage();
+        this.data = data;
+    }
+
+    public ShopResult(T data,String msg,Integer code){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+
+    public static <T> ShopResult<T> success(T data){
+        return new ShopResult<>(data, ResultEnum.SUCCESS);
+    }
+
+    public static <T> ShopResult<T> fail(T data){
+        return new ShopResult<>(data, ResultEnum.FAIL);
+    }
+
+    public static <T> ShopResult<T> fail(ResultEnum resultEnum){
+        return new ShopResult<>(null, resultEnum);
+    }
+
+    public static <T> ShopResult<T> fail(Integer code, String msg){
+        return new ShopResult<>(null, msg, code);
+    }
+}
