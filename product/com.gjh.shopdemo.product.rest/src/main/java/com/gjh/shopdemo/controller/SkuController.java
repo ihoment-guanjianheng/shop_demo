382c5414ac6e99e421e09b299da956367d7d7fa5
guanjianheng <jianheng.guan@govee.com>
2026-04-17 18:01:44 +0800

    修正请求路径

--- product/com.gjh.shopdemo.product.rest/src/main/java/com/gjh/shopdemo/controller/SkuController.java
+++ product/com.gjh.shopdemo.product.rest/src/main/java/com/gjh/shopdemo/controller/SkuController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/sku")
+@RequestMapping("/sku")
 public class SkuController {
 
     @Autowired
